6993c7f2aa8ccadc0864e6cf761bcef40fab70b6
karladuran28 <kaduran1998@gmail.com>
2022-07-08 17:43:44 -0500

    sass basic outline

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+export const App = () => {
+  return (
+    //esto es temporal, manana se construyen las ventanas
+    //juntando los componentes creados :)
+    <>
+      <h1>Algunos Componentes</h1>
+      <hr />
+      <h2>Componentes generales</h2>
+      <hr />
+      <h2>Componentes compradores</h2>
+      <hr />
+      <h2>Componentes proveedores</h2>
+      <hr />
+      <h2>Componentes administradores</h2>
+      <hr />
+    </>
+  )
+}
