4e95868c3ed89d2367f355a835144feebe94b9d6
aumoscos <aumoscos@espol.edu.ec>
2023-09-13 09:05:04 -0500

    Funcionalidad aceptar y negar registros, pendiente arreglar popups

--- src/administradores/components/CardSolicitudRegistro.jsx
+++ src/administradores/components/CardSolicitudRegistro.jsx
@@ -1,8 +1,24 @@
 import { useNavigate } from "react-router-dom";
-
+import {PopupAceptado} from "./PopupAceptado"
+import React, { useState } from 'react';
 export const CardSolicitudRegistro = ({solicitud}) => {
     const dateObj = new Date(solicitud.FechaSolicitud);
     const navigate = useNavigate();
+
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
+  const handleConfirm = () => {
+    closePopup();
+  };
     
     const onClickSolicitud = () => {
         console.log("Aceptando solicitud")
@@ -19,17 +35,35 @@ export const CardSolicitudRegistro = ({solicitud}) => {
         second: 'numeric',
       };
 
-    const onAceptarSolicitud = () => {
-      console.log("Aceptando solicitud")
+    const onAceptarSolicitud = async() => {
+      openPopup();
+      const body = solicitud;
+      const postresp = await fetch(`http://localhost:4000/api/v1/usuarios`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+      });
+      const data = await postresp.json()
+      console.log(data);
+      const delresp = await fetch(`http://localhost:4000/api/v1/solicitudregistro/${solicitud.IdSolicitud}`, {
+        method: 'DELETE',
+      });
+      
     }
   
-    const onRechazarSolicitud = () => {
-      console.log("Rechazando solicitud")
+    const onRechazarSolicitud = async() => {
+      const body = solicitud;
+      const delresp = await fetch(`http://localhost:4000/api/v1/solicitudregistro/${solicitud.IdSolicitud}`, {
+        method: 'DELETE',
+      });
     }
   
     return (
       <div className="cardSolicitudContainer" onClick={onClickSolicitud}>
-        <div className="cardSolicitudContainer--datosUser">
+         <PopupAceptado isOpen={isPopupOpen} onConfirm={handleConfirm} />
+        <div className="cardSolicitudContainer--datosUser" >
           <p className="paragraph"><b>{solicitud.Nombre}</b></p>
           <p className="paragraph">{solicitud.Email}</p>
           <p className="paragraph">Fecha solicitud: {dateObj.toLocaleString(undefined, options)} </p>
@@ -41,7 +75,7 @@ export const CardSolicitudRegistro = ({solicitud}) => {
           >
             Aceptar
           </button>
-  
+         
           <button
             onClick={onRechazarSolicitud}
             className="cardSolicitudContainer--botonesBox--btnRechazar"
